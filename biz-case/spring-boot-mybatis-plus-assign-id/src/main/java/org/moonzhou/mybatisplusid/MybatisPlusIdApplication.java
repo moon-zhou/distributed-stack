946e7f10c03febdee86a44e740f1aecef3d8c430
moon-zhou <ayimin1989@163.com>
2023-03-21 21:33:32 +0800

    feat: mybatis plus assign id high concurrency
    reconstruct

---
+++ biz-case/spring-boot-mybatis-plus-assign-id/src/main/java/org/moonzhou/mybatisplusid/MybatisPlusIdApplication.java
@@ -0,0 +1,13 @@
+package org.moonzhou.mybatisplusid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPlusIdApplication.class, args);
+	}
+
+}
